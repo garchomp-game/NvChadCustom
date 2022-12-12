4b75e235df4c2cbdfa537debf9cfa4d9502e41bb
garchomp-game <otoka.japanesedrum@gmail.com>
2022-12-12 14:35:44 +0900

    fix

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
-vim.opt.mouse = ""
+vim.opt.sessionoptions:append({"resize"})
+local cnk = vim.fn.getftype("mysession.viminfo")
+if cnk ~= "" then
+  vim.cmd('rviminfo! mysession.viminfo')
+end
 vim.opt.helplang = "ja,en"
+vim.opt.mouse = ""
 vim.opt.cursorline = ""
 vim.opt.relativenumber = ""
 vim.opt.cursorcolumn = ""
 vim.g.ctrlp_use_migemo = 1
 vim.g.laststatus = 2
-vim.opt.sessionoptions:append({"resize"})
-local cnk = getftype("myusession.viminfo")
-if cnk ~= "" then
-  vim.cmd('rviminfo! mysession.viminfo')
-end
