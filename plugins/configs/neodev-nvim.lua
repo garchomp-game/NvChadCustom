7ddf420fd879ab11d91a2c2f3c248651225b5510
garchomp-game <otoka.japanesedrum@gmail.com>
2022-12-21 22:53:47 +0900

    後でneodevのlibraryをどうにかする

--- plugins/configs/neodev-nvim.lua
+++ plugins/configs/neodev-nvim.lua
@@ -5,14 +5,38 @@ require("neodev").setup({
 
 -- then setup your lsp server as usual
 local lspconfig = require('lspconfig')
+-- ---@param plugins string[]
+-- ---@return string[]
+-- local function library(plugins)
+--   local paths = {}
+--   for _, plugin in ipairs(plugins) do
+--     local path = vim.fn["dein#get"](plugin).path
+--     if vim.bool_fn.isdirectory(path .. "/lua") then
+--       table.insert(paths, path)
+--     end
+--   end
+--   table.insert(paths, vim.fn.stdpath("config"))
+--   return paths
+-- end
 
 -- example to setup sumneko and enable call snippets
 lspconfig.sumneko_lua.setup({
   settings = {
     Lua = {
-      completion = {
-        callSnippet = "Replace"
-      }
-    }
-  }
+      format = {
+        enable = false,
+      },
+      diagnostics = {
+        globals = { "vim", "describe", "it" },
+      },
+      runtime = {
+        version = "LuaJIT",
+        path = { "lua/?.lua", "lua/?/init.lua" },
+      },
+      -- workspace = {
+      --   library = library({ "plenary.nvim", "nvim-cmp", "sqlite.lua" }),
+      --   checkThirdParty = false,
+      -- },
+    },
+  },
 })
